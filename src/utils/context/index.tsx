5a5a0f94c7074c1107c98719055d376f727a0bde
ismael200code <ismael.200code@gmail.com>
2026-07-31 23:21:25 +0300

    Suppression data test in the context

--- src/utils/context/index.tsx
+++ src/utils/context/index.tsx
@@ -6,21 +6,8 @@ import { useState } from "react";
 function DataProvider({children}) {
   
   const [data,setData] = useState<DataTypeState>({
-    etudiant: [
-      {id:1,coef:5,name:"something 1", data: [
-        {name:"matiere 1",coef:1,difficultyExam:"good",difficultyTest:"good",id:1,unitesId:1},
-        {name:"matiere 2",coef:1,difficultyExam:"good",difficultyTest:"good",id:2,unitesId:1}
-      ]},
-      {id:2,coef:5,name:"something 2", data: [
-        {name:"matiere 3",coef:1,difficultyExam:"good",difficultyTest:"good",id:1,unitesId:2},
-        {name:"matiere 4",coef:1,difficultyExam:"good",difficultyTest:"good",id:2,unitesId:2}
-      ]},
-    ],
-    eleve: [
-      {id:1,coef:5,difficultyMoyenne:"good",name:"matiere 1"},
-      {id:2,coef:5,difficultyMoyenne:"good",name:"matiere 2"},
-      {id:3,coef:5,difficultyMoyenne:"good",name:"matiere 3"},
-    ],
+    etudiant: [],
+    eleve: [],
     moyenneEleve: 0,
     moyenneEtudiant: 0
   })
