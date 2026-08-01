8cdb4a7c10507688fe33b67735bb21b22c91c2a8
ismael200code <ismael.200code@gmail.com>
2026-08-01 08:21:44 +0300

    Implementation of logo and favicorn

--- src/Components/Headers/index.tsx
+++ src/Components/Headers/index.tsx
@@ -1,11 +1,12 @@
-
+import Logo from "../../assets/logo.png"
 
 
 function Headers () {
 
   return (
-    <header className="mb-3 border-b sticky top-0 bg-background">
-      <h2 className="font-bold text-3xl text-center">CalcNotes <span className="italic font-medium">v1.3</span></h2>
+    <header className="mb-3 border-b sticky top-0 bg-background flex justify-center items-center h-10">
+      <img src={Logo} alt="" className="h-10"/>
+      <h2 className="font-bold text-3xl text-center">Calc<span>Notes</span> <span className="italic font-medium">v1.3</span></h2>
     </header>
   )
 }
