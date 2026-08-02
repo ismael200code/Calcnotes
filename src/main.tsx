fb8d4f313dc76a8b6730d8f392757a60a2c60847
ismael200code <ismael.200code@gmail.com>
2026-08-02 23:40:07 +0300

    Add google analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactGA from 'react-ga4'
 import './index.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
@@ -8,11 +9,15 @@ import NotFound from './pages/Errors'
 import Configuration from './pages/Configuration'
 import Resultat from './pages/Resultats'
 import DataProvider from './utils/context'
+import { AnalyticsTracker } from './utils/AnaliticsTracker/intex'
+
+ReactGA.initialize(import.meta.env.VITE_GA_ID)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <DataProvider>
       <Router>
+        <AnalyticsTracker/>
         <Headers/>
         <Routes>
           <Route path='/' element={<Home/>} />
